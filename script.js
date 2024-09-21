d6ed6c7e7e3e0fdbeac1838bc6ca16f32634e5af
bonny-chen81 <bonnychen.tw@gmail.com>
2024-09-21 13:47:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var map = L.map('map', {
     center: [22.604964, 120.300476],
     zoom: 16
 });
-L.tileLayer('https://dmap.ncdr.nat.gov.tw/1109/map/', {
-    attribution: '&copy; <a href="https://dmap.ncdr.nat.gov.tw/1109/map/">OpenStreetMap</a> contributors'
+L.tileLayer('https://www.openstreetmap.org/copyright', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 document.getElementById("Floodprevention").addEventListener("click", function() {
