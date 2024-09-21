b977435bfdf045b35632d4e656a50e95bb29837a
bonny-chen81 <bonnychen.tw@gmail.com>
2024-09-21 18:03:35 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,11 +1,4 @@
-//淹水潛勢圖
-var map = L.map('map', {
-    center: [22.604964, 120.300476],
-    zoom: 16
-});
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+
 
 
 //按鈕區
