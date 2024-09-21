46892f68335266066c36f81bddff44803abac9ca
bonny-chen81 <bonnychen.tw@gmail.com>
2024-09-21 14:26:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var map = L.map('map', {
     center: [22.604964, 120.300476],
     zoom: 16
 });
-L.tileLayer('https://www.openstreetmap.org/copyright', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
